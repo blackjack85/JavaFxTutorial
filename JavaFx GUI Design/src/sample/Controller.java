036360e81b48384874605136cf7c49a04fc09db5
SSelvaratnam <satija.selva@gmail.com>
2016-01-15 02:52:04 +0100

    JavaFx with simple fxml and controller sample

--- JavaFx GUI Design/src/sample/Controller.java
+++ JavaFx GUI Design/src/sample/Controller.java
@@ -1,4 +1,15 @@
 package sample;
 
+import javafx.scene.control.Button;
+
 public class Controller {
+
+    public Button button;
+
+    public void handleButtonClick()
+    {
+        System.out.println("run some code that user can't see...000xxx");
+        button.setText("stop touching me...x+2#");
+
+    }
 }
--- JavaFx GUI Design/src/sample/Main.java
+++ JavaFx GUI Design/src/sample/Main.java
@@ -4,14 +4,19 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.stage.Stage;
 
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
+
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
+
+        primaryStage.setTitle("Titel of Monitor");
+
+
         primaryStage.setScene(new Scene(root, 300, 275));
         primaryStage.show();
     }
